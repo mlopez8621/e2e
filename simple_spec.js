153fcf47a60eabcae0dc90da21c921acba2a0499
mlopeza1 <mlopeza1@ucentral.edu.co>
2019-08-25 14:07:04 -0500

    3. Pruebe como dirigirse a la página de un profesor

--- simple_spec.js
+++ simple_spec.js
@@ -24,10 +24,16 @@ describe('Los estudiantes login', function() {
       	cy.contains("Error: Ya existe un usuario registrado con el correo 'ml.lopeza@uniandes.edu.co'")
       	//2. Pruebe la funcionalidad de búsqueda de profesores
       	cy.visit('https://losestudiantes.co')
+      	
         cy.contains('cuenta')
         cy.contains('Busca un profesor o materia...').click()
         cy.get('.Select-input').find('input[role="combobox"]').type("Mario Linares Vasquez").click()
-        cy.contains('Mario Linares Vasquez')
+        
+        //3. Pruebe como dirigirse a la página de un profesor
+        cy.visit('https://losestudiantes.co')
+      	cy.contains('Alfabético').click()
+        cy.contains('Adolfo Jose Quiroz Salazar').click()
+
 
     })
 })
